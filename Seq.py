f02f8aa245412722511de974c77cdc743dfed066
dafnelp <dafnelozano88@hotmail.com>
2019-05-06 00:00:32 +0200

    final project

--- Seq.py
+++ Seq.py
@@ -9,7 +9,7 @@ class Seq:
 
     def perc(self, base):
         if base == "A":
-            perc_a = "{}".format(round(100.0 * self.strbases.count('A') / len(self.strbases), 1))
+            perc_a = "{} %".format(round(100.0 * self.strbases.count('A') / len(self.strbases), 1))
             return perc_a
 
         elif base == "C":
@@ -17,9 +17,9 @@ class Seq:
             return perc_c
 
         elif base == "T":
-            perc_t = "{}".format(round(100.0 * self.strbases.count('T') / len(self.strbases), 1))
+            perc_t = "{} %".format(round(100.0 * self.strbases.count('T') / len(self.strbases), 1))
             return perc_t
 
         elif base == "G":
-            perc_g = "{}".format(round(100.0 * self.strbases.count('G') / len(self.strbases), 1))
+            perc_g = "{} %".format(round(100.0 * self.strbases.count('G') / len(self.strbases), 1))
             return perc_g
